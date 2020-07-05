277ebba95df3fda30978d34fca7ad221b60a45fc
hhhh8815 <55180599+hhhh8815@users.noreply.github.com>
2020-07-05 11:20:20 +0800

    完成計數器

--- counter.js
+++ counter.js
@@ -4,43 +4,46 @@ $(document).ready(function () {
     // 當視窗 捲動時
     $(window).scroll(function () {
 
-        // var 變數 = 資料
-        var offset = $("#test").offset();       // 取得元素位移資料
-        var top = offset.top;                   // 上方
-        var left = offset.left;                 // 左方
 
-        // console.log("元素的上方：" + top);
+        $("*").each(function (index, element) {
 
-        var windowTop = $(window).scrollTop();
+            // element == this
+            // var 變數 = 資料
+            var offset = $(this).offset();       // 取得元素位移資料
+            var top = offset.top;                   // 上方
+            var left = offset.left;                 // 左方
 
-        // console.log("視窗的上方：" + windowTop);
+            // console.log("元素的上方：" + top);
 
-        // 取得參數：數字、時間與位移
-        var number = $("#test").attr("data-counter-number");
-        var time = $("#test").attr("data-counter-time");
-        var offset = $("#test").attr("data-counter-offset");
+            var windowTop = $(window).scrollTop();
 
-        //console.log("數字：" + number);
-        //console.log("時間：" + time);
-        //console.log("位移：" + offset);
+            // console.log("視窗的上方：" + windowTop);
 
-        // id  ()  {}
-        // 如果 發生什麼事 就...
-        // if (條件) { 程式 }
-        if (windowTop > top) {
-            // console.log("開始執行動畫")
-        }
+            // 取得參數：數字、時間與位移
+            var number = $(this).attr("data-counter-number");
+            var time = $(this).attr("data-counter-time");
+            var offset = $(this).attr("data-counter-offset");
 
-        $("#test").animate({
-            num: number
-        }, {
-            duration: parseInt(time),
-            step: (now) => {
-                $("#test").text(Math.floor(now));
+            //console.log("數字：" + number);
+            //console.log("時間：" + time);
+            //console.log("位移：" + offset);
+
+            // id  ()  {}
+            // 如果 發生什麼事 就...
+            // if (條件) { 程式 }
+            if (windowTop > top - offset) {
+                // console.log("開始執行動畫")
+
+
+                $(this).animate({
+                    num: number
+                }, {
+                    duration: parseInt(time),
+                    step: (now) => {
+                        $(this).text(Math.floor(now));
+                    }
+                })
             }
-        })
+        });
     });
-
-
-
 });
